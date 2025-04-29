53faa2f3c1020535c99bf4256c237a47f7eb505d
Nilava <nilavabiswas93@gmail.com>
2025-04-30 00:51:07 +0530

    .env template added

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import chromadb
 chroma = chromadb.HttpClient(host="localhost", port=8000)
 collection = chroma.get_collection("fragments_on_machines")  # Get the collection by name
+chroma.heartbeat()
+print("ChromaDB is running and the collection is accessible.")
